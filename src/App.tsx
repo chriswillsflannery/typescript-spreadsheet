74163fdd6d304396609704b12f0c1a1292ac4445
Chris Flannery <chriswillsflannery@gmail.com>
2020-02-21 11:22:53 -0500

    working through cell state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Column from './components/Column';
-import styled from 'styled-components';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,10 +7,10 @@ const App: React.FC = () => {
   const [numRows, setNumRows] = useState(6);
   const [numColumns, setNumColumns] = useState(6);
 
-  const StyledGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(${numColumns}, 1fr);
-  `;
+  const styles_grid = {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${numColumns}, 1fr)`
+  }
 
   const columns = [];
   for (let i = 0; i < numColumns; i++) {
@@ -20,9 +19,9 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <StyledGrid>
+      <main style={styles_grid}>
         {columns}
-      </StyledGrid>
+      </main>
       <div className="Controller">
         <button onClick={() => setNumRows(numRows + 1)}>Add Row</button>
         <button onClick={() => setNumColumns(numColumns + 1)}>Add Column</button>
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   col: number;
@@ -7,16 +7,35 @@ interface Props {
 
 const Cell: React.FC<Props> = ({ col, row }) => {
 
+  const [formVal, setFormVal] = useState('');
+
   const alpha = 'abcdefghijklmnopqrstuvwxyz';
   let bgColor;
   if (col === 0 || row === 0) bgColor = 'rgba(255,255,255,0.1)';
 
+  const handleChange = (e: any) => {
+    setFormVal(e.target.value);
+  }
+
   return (
-    <div style={{ backgroundColor: bgColor }} className="grid-cell">
+    <div
+      id={`cell${col}${row}`}
+      style={{ backgroundColor: bgColor }}
+      className="grid-cell">
       {(col === 0 && row === 0) && ' '}
-      {(col === 0 && row !== 0) && alpha[(row - 1) % 26]}
-      {(col !== 0 && row === 0) && alpha[(col - 1) % 26]}
-      {(col !== 0 && row !== 0) && ' '}
+      {(col === 0 && row !== 0) && <p>{alpha[(row - 1) % 26]}</p>}
+      {(col !== 0 && row === 0) && <p>{alpha[(col - 1) % 26]}</p>}
+      {(col !== 0 && row !== 0) && (
+        <form className="grid-form">
+          <input
+            className="grid-form-input"
+            type="text"
+            placeholder=""
+            onChange={handleChange}
+            value={formVal}
+          />
+        </form>
+      )}
     </div>
   )
 }
